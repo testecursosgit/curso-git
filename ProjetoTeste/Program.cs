284cefcce6009769618f71404d2980c6590e12bb
Daniel Lopes <danieljfsantos@gmail.com>
2020-01-16 22:27:17 -0300

    Mensagem git

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -5,7 +5,7 @@ namespace ProjetoTeste {
         static void Main(string[] args) {
 
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Git");
             Console.ReadLine();
         }
     }
