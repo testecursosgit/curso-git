815dea5c1490bae3c50b219605c31ec8c851210e
Daniel Lopes <danieljfsantos@gmail.com>
2020-01-20 20:40:24 -0300

    erros

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,11 +7,13 @@ namespace ProjetoTeste {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Git");
             Console.WriteLine("Daniel");
-            Console.ReadLine();
+            
 
             for (int i = 0; i < 10; i++) {
                 Console.WriteLine("C#");
             }
+
+            Console.ReadLine();
         }
     }
 }
